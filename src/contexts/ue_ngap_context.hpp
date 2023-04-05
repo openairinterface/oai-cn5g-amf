be14c0f32864849795550c86f6b638da5f9b1055
Tien Thinh NGUYEN <Tien-Thinh.Nguyen@eurecom.fr>
2023-04-05 09:50:14 +0200

    Fix issue after merging

--- src/contexts/ue_ngap_context.hpp
+++ src/contexts/ue_ngap_context.hpp
@@ -47,7 +47,7 @@ class ue_ngap_context {
     release_cause       = {};
     gnb_assoc_id        = {};
     target_gnb_assoc_id = {};
-    ueContextRequest    = false;
+    ue_context_request  = false;
     s_tmsi_5g           = {};
     s_setid             = {};
     s_pointer           = {};
@@ -55,8 +55,8 @@ class ue_ngap_context {
     tai                 = {};
     ng_ue_state         = NGAP_UE_INVALID_STATE;
     ncc                 = 0;
-    initial_ue_msg.buf    = new uint8_t[BUFFER_SIZE_1024];
-    initial_ue_msg.size   = 0;
+    initial_ue_msg.buf  = new uint8_t[BUFFER_SIZE_1024];
+    initial_ue_msg.size = 0;
   }
 
   virtual ~ue_ngap_context() {
@@ -90,7 +90,7 @@ class ue_ngap_context {
 
   OCTET_STRING_t initial_ue_msg;  // for AMF re-allocation
 
-  //Release Command Cause and source gNB ID in case of HO
+  // Release Command Cause and source gNB ID in case of HO
   Ngap_CauseRadioNetwork_t release_cause;
   uint32_t release_gnb;
 };
