e89ad5695131bcc3e62a1e5e9cefcd929a41aff1
Tien Thinh NGUYEN <Tien-Thinh.Nguyen@eurecom.fr>
2024-05-24 11:11:49 +0200

    Move get_amf_id function from AMF to common utils

--- src/utils/amf_conversions.hpp
+++ src/utils/amf_conversions.hpp
@@ -93,17 +93,5 @@ class amf_conv : public oai::utils::conv {
       const std::string& mcc, const std::string& mnc, const std::string& msin);
   static bool string_2_masked_imeisv(
       const std::string& str, BIT_STRING_t& imeisv);
-  static void get_amf_id(
-      uint8_t amf_region_id, uint16_t amf_set_id, uint8_t amf_pointer,
-      uint32_t& amf_id);
-  static void get_amf_id(
-      uint8_t amf_region_id, uint16_t amf_set_id, uint8_t amf_pointer,
-      std::string& amf_id);
-  static void get_amf_id(
-      const std::string& amf_region_id, const std::string& amf_set_id,
-      const std::string& amf_pointer, uint32_t& amf_id);
-  static void get_amf_id(
-      const std::string& amf_region_id, const std::string& amf_set_id,
-      const std::string& amf_pointer, std::string& amf_id);
 };
 #endif /* FILE_CONVERSIONS_HPP_SEEN */
