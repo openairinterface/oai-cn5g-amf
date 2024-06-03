0a4a7b9388beef2311ce0a10fe877ae41380c2cc
Raphael Defosseux <raphael.defosseux@openairinterface.org>
2024-06-03 11:50:36 +0200

    style(logger): fixing the formatting
    Signed-off-by: Raphael Defosseux <raphael.defosseux@openairinterface.org>

--- src/common/logger.hpp
+++ src/common/logger.hpp
@@ -76,7 +76,7 @@ class Logger : public oai::logger::logger_common {
   static void set_lttng(bool isLttngActive) {
     oai::logger::logger_registry::set_lttng_is_active(isLttngActive);
   }
-  
+
   static bool should_log(spdlog::level::level_enum level) {
     return oai::logger::logger_registry::should_log(level);
   }
