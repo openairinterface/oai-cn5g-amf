91b80aca88442c0ec1da3ead343d3227a36ef3f0
Tien Thinh NGUYEN <Tien-Thinh.Nguyen@eurecom.fr>
2026-02-06 11:06:32 +0100

    Code cleanup

--- src/sbi/amf_http2_server.cpp
+++ src/sbi/amf_http2_server.cpp
@@ -63,8 +63,8 @@ void amf_http2_server::start() {
           amf_cfg->get_tls_config().get_cert_key_path() + "/amf.key";
       std::string certificate_file =
           amf_cfg->get_tls_config().get_cert_certificate_path() + "/amf.crt";
-      Logger::amf_server().error("THINH %s", key_file);
-      Logger::amf_server().error("THINH %s", certificate_file);
+      Logger::amf_server().debug("Keyfile %s", key_file);
+      Logger::amf_server().debug("Certificate_file %s", certificate_file);
 
       tls.use_private_key_file(key_file, boost::asio::ssl::context::pem);
       tls.use_certificate_chain_file(certificate_file);
