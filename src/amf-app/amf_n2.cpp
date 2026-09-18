d8df81b183c3b829934675426ce53e72e090817a
Tien-Thinh NGUYEN <Tien-Thinh.Nguyen@eurecom.fr>
2026-09-18 23:28:18 +0200

    Initialize bstring before using
    Signed-off-by: Tien-Thinh NGUYEN <Tien-Thinh.Nguyen@eurecom.fr>

--- src/amf-app/amf_n2.cpp
+++ src/amf-app/amf_n2.cpp
@@ -2189,7 +2189,6 @@ void amf_n2::handle_itti_message(
 
   PduSessionResourceHandoverList handoverList = {};
   std::vector<PduSessionResourceItem> handoverItemList;
-  PduSessionResourceItem handoverItem = {};
 
   // TODO: wait for response from SMF and transfer T-RAN N3 information/ or
   // T-UPF to the source gNB
@@ -2214,6 +2213,7 @@ void amf_n2::handle_itti_message(
         pdu_session_id.set(pdu_session_id_value);
         OCTET_STRING_fromBuf(
             &handoverCommandTransfer, n2_sm.c_str(), n2_sm.length());
+        PduSessionResourceItem handoverItem = {};
         handoverItem.set(pdu_session_id, handoverCommandTransfer);
         if (handoverCommandTransfer.buf) {
           free(handoverCommandTransfer.buf);
