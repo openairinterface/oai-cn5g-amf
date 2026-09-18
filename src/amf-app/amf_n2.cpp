4f6e912fb448d22686ef93c0814a773cb3164003
Tien-Thinh NGUYEN <Tien-Thinh.Nguyen@eurecom.fr>
2026-09-18 14:36:01 +0200

    Initialize bstring before using
    Signed-off-by: Tien-Thinh NGUYEN <Tien-Thinh.Nguyen@eurecom.fr>

--- src/amf-app/amf_n2.cpp
+++ src/amf-app/amf_n2.cpp
@@ -1783,14 +1783,21 @@ void amf_n2::handle_itti_message(
   if (!itti_msg->ue_radio_cap_info_ind) return;
   uint64_t amf_ue_ngap_id = itti_msg->ue_radio_cap_info_ind->getAmfUeNgapId();
   uint32_t ran_ue_ngap_id = itti_msg->ue_radio_cap_info_ind->getRanUeNgapId();
-  OCTET_STRING_t ue_radio_cap;
+  OCTET_STRING_t ue_radio_cap = {};
   itti_msg->ue_radio_cap_info_ind->getUeRadioCapability(ue_radio_cap);
 
   // Store UE Radio Capability in UE NGAP Context
   std::shared_ptr<ue_ngap_context> unc = {};
-  if (!ran_ue_id_2_ue_ngap_context(ran_ue_ngap_id, gc->gnb_id, unc)) return;
+  if (!ran_ue_id_2_ue_ngap_context(ran_ue_ngap_id, gc->gnb_id, unc)) {
+    oai::utils::utils::free_wrapper((void**) &ue_radio_cap.buf);
+    return;
+  }
 
-  unc->ue_radio_cap_ind = blk2bstr(ue_radio_cap.buf, ue_radio_cap.size);
+  // Release the previously stored UE Radio Capability (if any)
+  oai::utils::utils::bdestroy_wrapper(&unc->ue_radio_cap_ind);
+  if (ue_radio_cap.buf and (ue_radio_cap.size > 0))
+    unc->ue_radio_cap_ind = blk2bstr(ue_radio_cap.buf, ue_radio_cap.size);
+  oai::utils::utils::free_wrapper((void**) &ue_radio_cap.buf);
 }
 
 //------------------------------------------------------------------------------
